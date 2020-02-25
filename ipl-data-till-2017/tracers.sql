fde57de941d9842ce5ddf700cbd5766f0a61b2a9
slash-g5 <32359242+slash-g5@users.noreply.github.com>
2020-02-25 23:38:27 +0530

    Update tracers.sql

--- ipl-data-till-2017/tracers.sql
+++ ipl-data-till-2017/tracers.sql
@@ -13,3 +13,7 @@ where player.player_id = a1.bowler group by a1.a2 , player.player_name order by 
 select a1.a2 as fours,player.player_name from (select bowler,count(bowler) as a2 
 from ball_by_ball where runs_scored = '4' and season = '2010' group by bowler order by count(bowler) desc)as a1 , player 
 where player.player_id = a1.bowler group by a1.a2 , player.player_name order by a1.a2 desc;
+
+select a1.a2 as wickets,player.player_name from (select bowler,count(bowler) as a2 
+from ball_by_ball where bowler_wicket = '1' and season = '2010' group by bowler order by count(bowler) desc)as a1 , player 
+where player.player_id = a1.bowler group by a1.a2 , player.player_name order by a1.a2 desc;
