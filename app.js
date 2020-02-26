98d43a5530d008144d5b0850dab148afc7a0ce29
Mycodek <kailash96kumawat@gmail.com>
2020-02-27 02:27:48 +0530

    succ

--- app.js
+++ app.js
@@ -25,62 +25,133 @@ const client = new Client({
     user: 'postgres',
     host: 'localhost',
     database: 'mydatabase',
-    password: 'XXXXXX',
+    password: 'psqlpsql',
     port: 5432,
 });
 client.connect();
 
 // routing
-let searchedP = 'all';
+let IndexData_obj = {
+    country_flag:false,
+    team_flag:false,
+    season_flag:false,
+    pro_flag : false,
+
+    country_name:'All Countries',
+    team_name:'All Teams',
+    season_year:'All Seasons',
+    property : 'Six/Four/Wickets'
+}
+
+function formQuery(){
+    if(IndexData_obj.season_flag && IndexData_obj.pro_flag){
+        if(IndexData_obj.property == 'Most_Six_Hitter') return Qrs.ss_sx(IndexData_obj.season_year);
+        else if(IndexData_obj.property == 'Most_Four_Hitter') return Qrs.ss_fs(IndexData_obj.season_year);
+        else if(IndexData_obj.property == 'Most_Wickets_Taker') return Qrs.ss_w(IndexData_obj.season_year);
+    }
+    return "SELECT * FROM Player";
+}
 
 app.get("/",async function(req,res){
     try{
-        qmain = Qrs.sp_byName('V Kohli');
-        console.log(searchedP);
-        if (searchedP != 'all'){
-            console.log("check");
-            qmain = Qrs.sp_byName(searchedP);
-        }
-        const PlayerResult = await client.query(qmain);
-        const tabresult = await client.query('SELECT * FROM team');
-        res.render("index.ejs",{title:'players',pdata : PlayerResult.rows[0],resTable:tabresult.rows});
-    }
-    catch(err){
-    }
-});
-
-app.get("/Players",async function(req,res){
-    try{
-        qmain = Qrs.sp_byName('V Kohli');
-        console.log(searchedP);
-        if (searchedP != 'all'){
-            console.log("check");
-            qmain = Qrs.sp_byName(searchedP);
-        }
-        const PlayerResult = await client.query(qmain);
-        const tabresult = await client.query('SELECT * FROM team');
-        res.render("index.ejs",{title:'players',pdata : PlayerResult.rows[0],resTable:tabresult.rows});
+        const q_comp = formQuery();
+        console.log(q_comp);
+        const allPlayer = await client.query(q_comp);
+        // console.log(allPlayer.rows);
+        const allSeason = await client.query('SELECT Season_Year FROM match GROUP BY Season_Year ORDER BY Season_Year');
+        const allTeam = await client.query('SELECT team_name FROM team');
+        const allCountry = await client.query('SELECT country_name FROM player GROUP BY country_name ORDER BY country_name');
+        res.render("index.ejs",{
+            allPlayer : allPlayer.rows,
+            allSeason : allSeason.rows,
+            allTeam   : allTeam.rows,
+            allCountry: allCountry.rows
+        });
     }
     catch(err){
         console.log("In catch");
     }
 });
+app.post('/search-multi-player',async function(req,res){
+    if (req.body.country_name != "All Countries"){
+        IndexData_obj.country_name = req.body.country_name;
+        IndexData_obj.country_flag = true;
+    }
+    if (req.body.team_name != "All Teams"){
+        IndexData_obj.team_name = req.body.team_name;
+        IndexData_obj.team_flag = true;
+    }
+    if(req.body.season_year != "All Seasons"){
+        IndexData_obj.season_year = req.body.season_year;
+        IndexData_obj.season_flag = true;
+    }
+    if(req.body.property != "Six/Four/Wickets") {
+        IndexData_obj.property = req.body.property;
+        IndexData_obj.pro_flag = true;
+    }
+    console.log(IndexData_obj);
+    console.log("msg in multi-player");
+    return res.redirect('/');
+});
+
+let searchedP = 'all';
+app.get("/Player",async function(req,res){
+    try{
+        qmain = Qrs.sp_byName('V Kohli');
+        console.log(searchedP);
+        if (searchedP != 'all'){
+            console.log("check");
+            qmain = Qrs.sp_byName(searchedP);
+        }
+        
+        const PlayerResult = await client.query(qmain);
+        const allPlayer = await client.query('SELECT * FROM player');
+        const allSeason = await client.query('SELECT Season_Year FROM match GROUP BY Season_Year ORDER BY Season_Year');
+        const allTeam = await client.query('SELECT team_name FROM team');
+        const allCountry = await client.query('SELECT country_name FROM player GROUP BY country_name ORDER BY country_name');
+        res.render("singlePlayer.ejs",{
+            title:'Players',
+            pdata : PlayerResult.rows[0],
+            resTable:allPlayer.rows,
+            allSeason : allSeason.rows,
+            allTeam : allTeam.rows,
+            allCountry : allCountry.rows
+        });
+    }
+    catch(err){
+        console.log("In catch");
+    }
+});
+
 app.post('/search-player',async function(req,res){
     // var post_data = req.body;
     console.log(req.body.name);
     searchedP = req.body.name;
-    return res.redirect('/Players');
+    console.log("msg in single player");
+    return res.redirect('/Player');
 });
 
-app.get("/Teams",async function(_err,result){
+
+app.get("/Teams",async function(_err,res){
     // res.send(fs.readFile("MovieClub.html"));
     try{
+        console.log("check89");
         const tabresult = await client.query('SELECT * FROM team');
-        result.render("index.ejs",{title : "Teams",resTable:tabresult.rows});
+        console.log("check91");
+        const allSeason = await client.query('SELECT Season_Year FROM match GROUP BY Season_Year ORDER BY Season_Year');
+        console.log("check93");
+        res.render("teamIndex.ejs",{
+            title:'Teams',
+            resTable:tabresult.rows,
+            allSeason : allSeason.rows
+        });
     }
     catch(err){
         console.log("In catch");
     }
+});
+app.get("*",(req,res)=>{
+    res.send("hey u'r lost");
 });
 
 app.listen(port,function(){
--- queries.js
+++ queries.js
@@ -1,9 +1,54 @@
 let teamDataSearch =function(arg1,arg2){
-    return 'Select * From Team WHERE Team.'+arg2+'='+"'"+arg1+"'";
+    return 'Select * From Team WHERE Team.'+arg2+'='+"'"+arg1+"';";
 }
 let playerDataSearch =function(arg1,arg2){
-    return 'Select * From player WHERE player.'+arg2+'='+"'"+arg1+"'";
+    return 'Select * From player WHERE player.'+arg2+'='+"'"+arg1+"';";
 }
+
+let SeasonStats_Sixes = function(arg1,arg2){
+    return 'select a1.a2 as sixes,player.player_name from (select striker,count(striker) as a2 from ball_by_ball,match where runs_scored =' +"'"+ arg1+"'" + 'and match.season_year = ' +"'"+ arg2 +"'" + ' and match.match_id = ball_by_ball.match_id  group by striker order by count(striker) desc)as a1 , player  where player.player_id = a1.striker group by a1.a2 , player.player_name order by a1.a2 desc;' ;
+}
+
+let SeasonStats_Fours = function(arg1,arg2){
+    return 'select a1.a2 as fours,player.player_name from (select striker,count(striker) as a2 from ball_by_ball,match where runs_scored =' + "'" +arg1+"'" + 'and match.season_year = ' +"'"+ arg2 +"'"+ ' and match.match_id = ball_by_ball.match_id  group by striker order by count(striker) desc)as a1 , player  where player.player_id = a1.striker group by a1.a2 , player.player_name order by a1.a2 desc;' ;
+}
+let SeasonStats_conceededFours = function(arg1,arg2)
+{
+    return 'select a1.a2 as fours,player.player_name from (select bowler,count(bowler) as a2 ' + 
+   ' from ball_by_ball,match where runs_scored = '+"'" + arg1 +"'"+' and match.season_year = ' + "'"+
+   arg2 +"'" +' and match.match_id = ball_by_ball.match_id group by bowler order by count(bowler) desc)as a1 , player ' +
+   ' where player.player_id = a1.bowler group by a1.a2 , player.player_name order by a1.a2 desc';
+}
+
+let SeasonStats_conceededSixes = function(arg1,arg2)
+{
+    return 'select a1.a2 as sixes,player.player_name from (select bowler,count(bowler) as a2 ' + 
+   ' from ball_by_ball,match where runs_scored = ' +"'" + arg1 +"'" +' and match.season_year = ' + "'" +
+   "'" +arg2 +"'"+' and match.match_id = ball_by_ball.match_id group by bowler order by count(bowler) desc)as a1 , player ' +
+   ' where player.player_id = a1.bowler group by a1.a2 , player.player_name order by a1.a2 desc;';
+}
+
+let SeasonStats_Wickets = function(arg1)
+{
+    return 'select a1.a2 as wickets,player.player_name from (select bowler,count(bowler) as a2 ' +
+    'from ball_by_ball,match where bowler_wicket = 1 and match.season_year = ' + "'" + arg1 +"'" + 'and match.match_id = ball_by_ball.match_id' +
+     'group by bowler order by count(bowler) desc)as a1 , player ' +
+   ' where player.player_id = a1.bowler group by a1.a2 , player.player_name order by a1.a2 desc;';
+}
+
+let TopScorers = function(arg1)
+{
+return  'create view runs_in_ball AS' +
+'select runs_scored ,striker,season_year from ball_by_ball,match where match.match_id= ball_by_ball.match_id' + ";" +
+'select a1.sum,player_name from ' +
+'(select sum(runs_scored),striker from runs_in_ball where  season_year = '+"'" + arg1 + "'" + 
+'group by striker order by sum(runs_scored) desc) as a1 , player where player.Player_Id = a1.striker ' +
+'group by a1.striker,a1.sum,player_name order by a1.sum desc' + ";" +
+'drop view runs_in_ball;';
+}
+
+
+
 module.exports = function (_arg){
     this.st_bySk = (arg)=>{return teamDataSearch(arg,'team_sk')}
     this.st_byId = (arg)=>{return teamDataSearch(arg,'team_id')}
@@ -16,5 +61,13 @@ module.exports = function (_arg){
     this.sp_byBattingHand = (arg)=>{return playerDataSearch(arg,'batting_hand')}
     this.sp_byBowlingHand = (arg)=>{return playerDataSearch(arg,'bowling_skill')}
     this.sp_byCountry = (arg)=>{return playerDataSearch(arg,'country_name')}
+    
+
+    this.ss_sx = (arg) => { return SeasonStats_Sixes('6',arg)}
+    this.ss_fs = (arg) => { return SeasonStats_Fours('4',arg)}
+    this.ss_cf = (arg) => {return SeasonStats_conceededFours('4' , arg)}
+    this.ss_cs = (arg) => {return SeasonStats_conceededSixes('6', arg)}
+    this.ss_w = (arg) => {return SeasonStats_Wickets(arg)}
+    this.topScr = (arg) => {return TopScorers(arg1)}
 
 }
